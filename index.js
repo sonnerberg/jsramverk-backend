ef08a128d2c75cb19ac12ac4ef3a6cf89eb2eee3
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2020-08-16 09:46:10 +0200

    Add colors to terminal output

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const app = require('./app')
 const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+// eslint-disable-next-line no-unused-vars
+const colors = require('colors')
 
 const server = http.createServer(app)
 
 server.listen(config.PORT, () => {
-  logger.info(`Server running on localhost:${config.PORT}`)
+  logger.info(`Server running on localhost:${config.PORT}`.yellow.bold)
 })
