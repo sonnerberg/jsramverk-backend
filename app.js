a054c827ab321d2f47799039f221c8ce8a47f365
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2020-08-16 09:46:19 +0200

    Use newer body-parser

--- app.js
+++ app.js
@@ -8,10 +8,8 @@ const {
   loginRouter,
   registerRouter,
 } = require('./routes/')
-const bodyParser = require('body-parser')
 
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.json()) // for parsing application/json
 
 app.use(cors())
 // don't show the log when it is test
