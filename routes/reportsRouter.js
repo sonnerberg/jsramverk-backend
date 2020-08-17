ad8b7b6e097fcdedd57a5bc78cae51c59ffadf22
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2020-08-17 13:04:41 +0200

    Add setup video outside of github

--- routes/reportsRouter.js
+++ routes/reportsRouter.js
@@ -1,8 +1,14 @@
 const reportsRouter = require('express').Router()
 const { getKmom, getKmoms, createKmom } = require('../controllers/reports')
+const path = require('path')
 
 reportsRouter.route('/').get(getKmoms).post(createKmom)
 
 reportsRouter.route('/week/:id').get(getKmom)
+
+reportsRouter.get('/setup.svg', function (req, res) {
+  res.setHeader('Content-Type', 'image/svg+xml')
+  res.sendFile(path.join(__dirname, '../setup.svg'))
+})
 
 module.exports = reportsRouter
