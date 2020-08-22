cb4ba8194fb429fb1dc6b6cad5d909414e63b375
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2020-08-22 20:37:47 +0200

    Use path
    Try to make systemd work on digital ocean

--- db/database.js
+++ db/database.js
@@ -1,9 +1,10 @@
 const sqlite3 = require('sqlite3').verbose()
+const path = require('path')
 
 module.exports = (function () {
   if (process.env.NODE_ENV === 'test') {
-    return new sqlite3.Database('./db/test.sqlite')
+    return new sqlite3.Database(path.join(__dirname, '/test.sqlite'))
   }
 
-  return new sqlite3.Database('./db/texts.sqlite')
+  return new sqlite3.Database(path.join(__dirname, '/texts.sqlite'))
 })()
