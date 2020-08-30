39ec7dceab2a704e5f114666c7062cddbfd871e9
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2020-08-30 14:55:17 +0200

    Fix unused variable

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 // eslint-disable-next-line no-unused-vars
 const colors = require('colors')
+colors.enable()
 
 const server = app.listen(config.PORT, () => {
   logger.info(`Server running on localhost:${config.PORT}`.yellow.bold)
