1be1cdb80241a19f87932fe53ea3fd5b2ab940ac
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2020-09-19 15:40:01 +0200

    Update script

--- db/update_kmom01.bash
+++ db/update_kmom01.bash
@@ -1,10 +1,25 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
-# Check if frontend directory exists
-FILE=../../jsramverk-frontend/src/README.md
-if [[ -f "$FILE" ]]; then
-    echo "$FILE" is present, updating...
-    /bin/cat "$FILE"  > ../reports/kmom01.md
+FRONTEND_README=../../jsramverk-frontend/src/README.md
+BACKEND_REPORT=../reports/kmom01.md
+
+if [[ -f "$FRONTEND_README" ]]; then
+    echo "$FRONTEND_README" is present, updating...
+
+    FRONTEND_README_TOUCHED=$(/usr/bin/env ls -l --time-style='+%s' ../../jsramverk-frontend/src/README.md | cut --delimiter=' ' --fields=6)
+    BACKEND_REPORT_TOUCHED=$(/usr/bin/env ls -l --time-style='+%s' ../reports/kmom01.md | cut --delimiter=' ' --fields=6)
+
+    if [[ "$FRONTEND_README_TOUCHED" > "$BACKEND_REPORT_TOUCHED" ]]; then
+        echo "$FRONTEND_README" is newer
+
+        /usr/bin/env cat "$FRONTEND_README"  > "$BACKEND_REPORT"
+        else
+
+        echo "$BACKEND_REPORT" is newer
+
+        /usr/bin/env cat "$BACKEND_REPORT" > "$FRONTEND_README"
+    fi
+
 else
-    echo "FILE" not present
+    echo "$FRONTEND_README" not present
 fi
